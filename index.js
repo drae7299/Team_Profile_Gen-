099eafe866ce97fd74d2544cbec7e5a513e3c11d
Andre Rodriguez <drae7299@gmail.com>
2021-04-17 18:16:28 -0400

    arranging folders

---
+++ index.js
@@ -0,0 +1,34 @@
+//Consts
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require()
+// require all object (classes employee..)
+//requirer
+//fs
+//team memebers open empty array( stores manager, employee oopbjects)
+//generate or getemployee const for function** Makes actual template(reads the array and filters it and filters it into the proper array and then for loop)
+
+
+
+//Logic
+// create teams times 4x
+//function to create our manager with inquirer package and ask questions from readme
+//attach .then to handle responses(inside .then pass responses as arguments to class objects)(console.log)
+//Save data to empy global array(push new object into new array in global)
+//function to writeFileSync
+//generateEmployee and pass in team members array from global consts
+
+
+
+
+
+//.filter method returns array for managers(with this, call function that makes a template for each class objects)
+//store mini templates into large array
+//.join ("") makes mini temp into one biggggg template
+//return temp vis writeFile and this writes to team html file
+//.html (use as an argument)
+
+// Test arguments 
+
+
+
