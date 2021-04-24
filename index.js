9d9fd151170fe85468776efb646cb54d6344918f
Andre Rodriguez <drae7299@gmail.com>
2021-04-24 00:08:23 -0400

    finished html temp for const

--- index.js
+++ index.js
@@ -168,3 +168,93 @@ const newEngineer = () => {
     const genArray = []
     const indexOne = 
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  <!DOCTYPE html>
+// <html lang="en">
+// <head>
+    
+// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+// <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+//     <link rel="stylesheet" href="/style/style.css">
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title></title>
+// </head>
+// <body>
+//     <div class="card-container">
+    
+//         <div class="card" style="width: 18rem;">
+//             <div class="card-body">
+//                 <h1 class="teamName"></h1>
+//                 <h2 class="card-title"></h2>
+//                 <h3 class="card-subtitle mb-2 text-muted"></h2>
+//             </div>
+//             <div class="card-bottom">
+//                 <p class="card-text">ID: </p>
+//                 <p class="card-text">Email: <a class="card-link" href="mailto:fsdfdsfd"></a></p>
+        
+//             <p></p>
+            
+//         </div>
+//         </div>
+        
+//     </div>
+//     </body>
+//     </html>
