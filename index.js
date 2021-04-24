326d36829bf2eac701c8f2e1baaa2d89f67ef29b
Andre Rodriguez <drae7299@gmail.com>
2021-04-24 00:46:09 -0400

    final changes to index js for functionality, made model class changes and small touch ups

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const newEmp = () => {
           newIntern();
           break;
         case "No":
-          generateTeam();
+          generateIt();
           break;
       }
     });
@@ -122,139 +122,115 @@ const newEngineer = () => {
       const id = answers.id;
       const email = answers.email;
       const github = answers.github;
-      const newEmployee = new Manager(name, id, email, github);
+      const newEmployee = new Engineer(name, id, email, github);
       empArray.push(newEmployee);
-      //new employee call back funtion to push data to array 
+      //new employee call back funtion to push data to array
       newEmp();
     });
 };
- const newIntern = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "What is the Interns name?",
-            name: "name",
-          },
-          {
-            type: "input",
-            message: "What is the Interns Employee ID?",
-            name: "id",
-          },
-          {
-            type: "input",
-            message: "What is the Interns email address?",
-            name: "email",
-          },
-          {
-            type: "input",
-            message: "What is the Interns School?",
-            name: "school",
-          },
-    ]).then((answers) => {
-        const name = answers.name;
-        const id = answers.id;
-        const email = answers.email;
-        const school = answers.school;
-        const newEmployee = new Manager(name, id, email, school);
-        empArray.push(newEmployee);
-        //new employee call back funtion to push data to array 
-        newEmp();
-      });
- };
+const newIntern = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the Interns name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the Interns Employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the Interns email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the Interns School?",
+        name: "school",
+      },
+    ])
+    .then((answers) => {
+      const name = answers.name;
+      const id = answers.id;
+      const email = answers.email;
+      const school = answers.school;
+      const newEmployee = new Intern(name, id, email, school);
+      empArray.push(newEmployee);
+      //new employee call back funtion to push data to array
+      newEmp();
+    });
+};
 
-
- const generateIt = () => {
-
-    const genArray = []
-    const indexOne = 
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  <!DOCTYPE html>
-// <html lang="en">
-// <head>
-    
-// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-// <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-//     <link rel="stylesheet" href="/style/style.css">
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title></title>
-// </head>
-// <body>
-//     <div class="card-container">
-    
-//         <div class="card" style="width: 18rem;">
-//             <div class="card-body">
-//                 <h1 class="teamName"></h1>
-//                 <h2 class="card-title"></h2>
-//                 <h3 class="card-subtitle mb-2 text-muted"></h2>
-//             </div>
-//             <div class="card-bottom">
-//                 <p class="card-text">ID: </p>
-//                 <p class="card-text">Email: <a class="card-link" href="mailto:fsdfdsfd"></a></p>
+const generateIt = () => {
+  const genArray = [];
+  const indexOne = 
+    `<!DOCTYPE html>
+     <html lang="en">
+     <head>
         
-//             <p></p>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+         <link rel="stylesheet" href="/style/style.css">
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+         <title>${empArray[0]}</title>
+     </head>
+     <body>
+         <div class="card-container">`
+    
+         genArray.push(indexOne);
+    for (i = 1; i < empArray.length; i++){
+        let pushed = `<div class="card" style="width: 18rem;">
+                 <div class="card-body">
+                    <h1 class="teamName">${empArray[0]}</h1>
+                     <h2 class="card-title">${empArray[i].name}</h2>
+                     <h3 class="card-subtitle mb-2 text-muted">Role: ${empArray[i].role}</h2>
+                 </div>
+                 <div class="card-bottom">
+                     <p class="card-text"> ID: ${empArray[i].id} </p>
+                     <p class="card-text"> Email: <a class="card-link" href="mailto: ${empArray[i].email}"> ${empArray[i].email}</a></p>
             
-//         </div>
-//         </div>
-        
-//     </div>
-//     </body>
-//     </html>
+    `
+    if (empArray[i].officeNumber) {
+        pushed += `
+        <p> Contact: ${empArray[i].officeNumber}</p>
+        `
+    }
+    if (empArray[i].github) {
+        pushed += `
+        <p>GitHub: <a href="https://github.com/${empArray[i].github}">${empArray[i].github}</a></p>
+        `
+    }
+    if (empArray[i].school) {
+        pushed += `
+        <p>School: ${empArray[i].school}</p>
+        `
+    }
+    pushed += `
+    </div>
+    </div>
+    `
+    genArray.push(pushed)
+}
+
+const prodFin = `
+</div>
+</body>
+</html>
+`
+genArray.push(prodFin);
+
+fs.writeFile('output\index.html', genArray.join(""), function (err) {
+    if (err) throw err
+    else {
+        console.log("Team Generated");
+        console.log('Success');
+    }
+})
+}
+init();
