e575b79572400670ffc76bb221335b64163ad3fd
Andre Rodriguez <drae7299@gmail.com>
2021-04-23 21:21:37 -0400

    made changes to file structure and reviwed the models made

---
+++ test/Employee.test.js
@@ -0,0 +1,9 @@
+// const Employee = require("../lib/Employee")
+
+// describe("Employee", () => {
+//     const testName = "Andre";
+//     const testEmail = "abc123@gmail.com";
+//     const testId = "28";
+//     const testRole = "Employee";
+//     var emp = 
+// });
