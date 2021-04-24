a94adb8172d95dd660899c899b706852d0c91811
Andre Rodriguez <drae7299@gmail.com>
2021-04-23 23:06:48 -0400

    added logic for new employee and manager, will do intern next to wrap that up

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//Global Consts
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+//Empty to push data from employee into 
 empArray = [];
 
+//Init to start app followed by prompted Q and A
 const init = () => {
     inquirer.prompt([
         {
@@ -20,8 +22,10 @@ const init = () => {
         newManager();
     })
 }
-
+//Manager prompts when entering new manager in
+//Manager Call back funtion
 const newManager = () => {
+    //promt user questions
     inquirer.prompt([
         {
             type: 'input',
@@ -38,47 +42,83 @@ const newManager = () => {
             message: "What is your team manager's email address?",
             name: 'email',
         },
-
         {
             type: 'input',
             message: "What is your team manager's office number?",
             name: 'officeNumber',
         },
+        //Consts scoped to user answers to be placed properly(WIll do this for most sets of prompts)
     ]).then((answers) => {
         const name = answers.name;
         const id = answers.id;
         const email = answers.email;
         const officeNumber = answers.officeNumber;
         const newEmployee = new Manager(name, id, email, officeNumber);
-        employeeArray.push(newEmployee);
+        empArray.push(newEmployee);
         
-	    newMember();
+	    newEmp();
     });
 };
 
-const newMember = () => { 
-
+const newEmp = () => { 
+//New employee call back funtion
     inquirer.prompt([ 
 
         {  
             type: 'list', 
-            message: 'Would you like to add any more additional new team members?', 
+            message: 'Would you like to add any more employees?', 
             choices: [ 
                 'Engineer', 
                 'Intern', 
-                'No (Finish and exit the application)',
+                'No',
             ], 
-            name: 'newMemberData' 
+            name: 'newEmpData' 
         }, 
-    ]) .then((answers)) => {
-        switch (answers.newMemberData) { 
+    ]) .then((answers) => {
+        switch (answers.newEmpData) { 
             case 'Engineer': 
                 newEngineer(); 
                 break; 
             case 'Intern': 
                 newIntern(); 
                 break; 
-            case 'No (Finish and exit the application)': 
+            case 'No': 
                 generateTeam();
                 break; 
         }; 
+    });
+};
+
+
+const newEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the Engineers name?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is the Engineers Employee ID?',
+            name: 'id',
+        },{
+            type: 'input',
+            message: 'What is the Engineers email address?',
+            name: 'email',
+        },{
+            type: 'input',
+            message: 'What is the Engineers Github Username?',
+            name: 'github',
+        },
+       
+    ]) .then((answers) => {
+        const name = answers.name;
+        const id = answers.id;
+        const email = answers.email;
+        const officeNumber = answers.officeNumber;
+        const newEmployee = new Manager(name, id, email, officeNumber);
+        empArray.push(newEmployee);
+        
+	    newEmp();
+});
+};
