044c365eee5b0a69be2393c1adcea08679728982
Andre Rodriguez <drae7299@gmail.com>
2021-04-27 17:19:33 -0400

    major changes to test files

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,9 +1,49 @@
-// const Employee = require("../lib/Employee")
+const Employee = require("../lib/Employee");
 
-// describe("Employee", () => {
-//     const testName = "Andre";
-//     const testEmail = "abc123@gmail.com";
-//     const testId = "28";
-//     const testRole = "Employee";
-//     var emp = 
-// });
+describe("Employee", () => {
+    it("Should create a member", => () {
+        const testName = "Andre";
+        const testEmail = "drae7299@gmail.com"
+        const testId = "399739"
+        const testRole = "Employee"
+        let e = new Employee(testName, testEmail, testId, testRole);
+
+        expect(e.name).toEqual(testName);
+        expect(e.email).toEqual(testEmail);
+        expect(e.id).toEqual(testId);
+        expect(e.role).toEqual(testRole);
+
+    }
+);
+
+describe("getName" , () =>  {
+
+    it("Should aquire a name", () => {
+        const test = "Name";
+        let eName = new Employee(test);
+        expect(eName.getName()).toEqual(test);
+
+
+    })
+});
+
+describe("getEmail" , () => {
+  
+    it("should aquire an email" , () => {
+        const test = "Email";
+        let eEmail = new Employee(test);
+        expect(eEmail.getName()).toEqual("Name" , test);
+    })
+});
+
+describe("getId", () => {
+
+    it("Should get an id", () => {
+        const test = "ID";
+        let eId = new Employee(test);
+        expect(eId.getId()).toEqual("Name", "Email" , test);
+    });
+});
+});  
+
+module.exports = Employee;
